48485d1d86bc0da42a53b7bd74d9ea0643f398dd
takato <leonardo.takato@gmail.com>
2023-07-02 17:23:20 -0300

    map e filter

--- curso/src/curso/aula4.clj
+++ curso/src/curso/aula4.clj
@@ -36,3 +36,28 @@
   (+ valor 3))
 
 (println (update precos 0 soma-3))
+
+(defn aplica-desconto?
+  [valor-bruto]
+  (> valor-bruto 100))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10%, se deve aplicar desconto com a função passada"
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println "map" (map valor-descontado precos))
+
+;; gerar de 0 ao 9
+(println (range 10))
+;; filtrar os numeros pares de 0 ao 9
+(println (filter even? (range 10)))
+
+(println "vetor" precos)
+(println "filter" (filter aplica-desconto? precos))
+
+(println "map apos o filter" (map valor-descontado (filter aplica-desconto? precos)))
