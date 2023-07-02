a93b2baffee45202309da5652cea40da08a7f839
takato <leonardo.takato@gmail.com>
2023-07-02 16:38:29 -0300

    high order functions, lambdas, anonymous functions

--- curso/src/curso/aula3.clj
+++ curso/src/curso/aula3.clj
@@ -17,13 +17,42 @@
   [valor-bruto]
   (> valor-bruto 100))
 
-(defn valor-descontado
-  "Retorna o valor com desconto de 10%"
+(defn mais-caro-que-100?
   [valor-bruto]
-  (if (aplica-desconto? valor-bruto)
+  (> valor-bruto 100))
+
+;; (defn valor-descontado
+;;   "Retorna o valor com desconto de 10%"
+;;   [valor-bruto]
+;;   (if (aplica-desconto? valor-bruto)
+;;     (let [taxa-de-desconto (/ 10 100)
+;;           desconto (* valor-bruto taxa-de-desconto)]
+;;       (- valor-bruto desconto))
+;;     valor-bruto))
+;; (valor-descontado 1000)
+
+;; refatorado: função como parametro (high order functions)
+(defn valor-descontado
+  "Retorna o valor com desconto de 10%, se deve aplicar desconto com a função passada"
+  [aplica? valor-bruto]
+  (if (aplica? valor-bruto)
     (let [taxa-de-desconto (/ 10 100)
           desconto (* valor-bruto taxa-de-desconto)]
       (- valor-bruto desconto))
     valor-bruto))
 
-(valor-descontado 1000)
+(println (valor-descontado mais-caro-que-100? 1000))
+(println (valor-descontado mais-caro-que-100? 100))
+
+;; CHECKPOINT: CONCEITOS
+;; high order functions
+;; imutabilidade
+
+;; with anonymous function
+(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 1000))
+(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 100))
+(println (valor-descontado #(> %1 100) 1000))
+(println (valor-descontado #(> %1 100) 100))
+;; como tem só um argumento, nem precisa de 1
+(println (valor-descontado #(> % 100) 1000))
+(println (valor-descontado #(> % 100) 100))
